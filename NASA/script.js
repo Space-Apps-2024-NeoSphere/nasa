3c0136f13db9916e7a7e88d4eb3079b68a356574
antony-jacob817 <antonyjacob817@gmail.com>
2024-10-06 22:54:53 +0530

    Add files via upload

--- NASA/script.js
+++ NASA/script.js
@@ -4,3 +4,36 @@ $(document).ready(function() {
     transitionDuration: 1000 // Adjust the transition duration as needed (in milliseconds)
   });
 });
+// Example JavaScript function to register a student
+
+async function registerStudent(username, email) {
+
+const response = await fetch('http://127.0.0.1:8000//api/register/', {
+
+method: 'POST',
+
+headers: {
+
+'Content-Type': 'application/json',
+
+},
+
+body: JSON.stringify({ username, email }),
+
+});
+
+if (response.ok) {
+
+const data = await response.json();
+
+console.log('Registration successful:', data);
+
+} else {
+
+const errorData = await response.json();
+
+console.error('Registration failed:', errorData);
+
+}
+
+} 
